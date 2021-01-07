a5fd6e89dfe2754c802f98d405584c117a01090a
Ali Parlakci <parlakciali@gmail.com>
2021-01-08 01:22:47 +0300

    the6 main file implemented

--- the6/aliparlakci_the6.cpp
+++ the6/aliparlakci_the6.cpp
@@ -7,7 +7,13 @@
 
 using namespace std;
 
+void run(vector<series> &seriesList, FavoriteSeries &favorites);
 void printMenu();
+void printAll(vector<series> &seriesList);
+void add(vector<series> &seriesList, FavoriteSeries &favorites);
+void remove(vector<series> &seriesList, FavoriteSeries &favorites);
+void printFavoriteSeries(FavoriteSeries &favorites);
+void printFavoriteActors(FavoriteSeries &favorites);
 void mergeEpisodesAndSeries(const vector<episode> &episodesList, vector<series> &seriesList);
 vector<series> readSeries(ifstream &seriesFile);
 vector<episode> readEpisodes(ifstream &episodesFile);
@@ -39,17 +45,12 @@ int main()
 
     FavoriteSeries favorites(name, surname);
 
-    for (int i = 0; i < seriesList.size(); i++)
-    {
-        favorites.addSeries(seriesList[i]);
-    }
-
-    favorites.displayFavoriteActors();
+    run(seriesList, favorites);
 
     return 0;
 }
 
-void run(vector<series> &seriesList)
+void run(vector<series> &seriesList, FavoriteSeries &favorites)
 {
     int choice = 0;
 
@@ -63,32 +64,29 @@ void run(vector<series> &seriesList)
         switch (choice)
         {
         case 1:
-            cout << "---" << endl;
+            printAll(seriesList);
             break;
 
         case 2:
-            cout << "---" << endl;
+            add(seriesList, favorites);
             break;
 
         case 3:
-            cout << "---" << endl;
-            break;
+            remove(seriesList, favorites);
 
         case 4:
-            cout << "---" << endl;
+            printFavoriteSeries(favorites);
             break;
 
         case 5:
-            cout << "---" << endl;
+            printFavoriteActors(favorites);
             break;
 
         case 6:
-            cout << "---" << endl;
             isEnded = true;
             break;
 
         default:
-            cout << "---" << endl;
             cout << "Invalid option!" << endl;
             break;
         }
@@ -107,6 +105,122 @@ void printMenu()
          << "6. Exit" << endl
          << "---" << endl
          << "Enter your choice: ";
+}
+
+void printAll(vector<series> &seriesList)
+{
+    FavoriteSeries all("", "");
+
+    for (int i = 0; i < seriesList.size(); i++)
+    {
+        all.addSeries(seriesList[i]);
+    }
+
+    all.displayFavoriteList();
+}
+
+void add(vector<series> &seriesList, FavoriteSeries &favorites)
+{
+    int code;
+    cout << "Enter code for the TV series you want to add: ";
+    cin >> code;
+
+    series correspondingSeries;
+    bool isFound = false;
+
+    for (int i = 0; i < seriesList.size() && !isFound; i++)
+    {
+        if (seriesList[i].seriesCode == code)
+        {
+            correspondingSeries = seriesList[i];
+            isFound = true;
+        }
+    }
+
+    if (!isFound)
+    {
+        cout << favorites.getFullName() << ", there is no such TV series in the database!" << endl;
+    }
+    else if (favorites.ifExists(code))
+    {
+        cout << favorites.getFullName() << ", you can't add this TV series because you already added it!" << endl;
+    }
+    else if (favorites.isConflict(correspondingSeries))
+    {
+        cout << favorites.getFullName() << ", you can't add this TV series because of a date and slot conflict!" << endl;
+    }
+    else
+    {
+        favorites.addSeries(correspondingSeries);
+        cout << favorites.getFullName() << ", ";
+        cout << correspondingSeries.seriesName;
+        cout << " added to your schedule." << endl;
+    }
+}
+
+void remove(vector<series> &seriesList, FavoriteSeries &favorites)
+{
+    int code;
+    cout << "Enter code for the TV series you want to drop: ";
+    cin >> code;
+
+    series correspondingSeries;
+    bool isFound = false;
+
+    for (int i = 0; i < seriesList.size() && !isFound; i++)
+    {
+        if (seriesList[i].seriesCode == code)
+        {
+            correspondingSeries = seriesList[i];
+            isFound = true;
+        }
+    }
+
+    if (favorites.isListEmpty())
+    {
+        cout << favorites.getFullName() << ", you did not add any TV series yet!" << endl;
+    }
+    else if (!isFound)
+    {
+        cout << favorites.getFullName() << ", there is no such TV series in the database!" << endl;
+    }
+    else if (!favorites.ifExists(code))
+    {
+        cout << favorites.getFullName() << ", there is no such TV series in your favorite list!" << endl;
+    }
+    else
+    {
+        favorites.dropSeriesSorted(code);
+        cout << favorites.getFullName() << ", ";
+        cout << correspondingSeries.seriesName;
+        cout << " has been dropped from your favorite list!" << endl;
+    }
+}
+
+void printFavoriteSeries(FavoriteSeries &favorites)
+{
+    if (favorites.isListEmpty())
+    {
+        cout << favorites.getFullName() << ", you did not add any TV series yet!" << endl;
+    }
+    else
+    {
+        cout << favorites.getFullName() << " here is your favorite TV series:" << endl;
+        favorites.displayFavoriteList();
+    }
+}
+
+void printFavoriteActors(FavoriteSeries &favorites)
+{
+    if (favorites.isListEmpty())
+    {
+        cout << favorites.getFullName() << ", you did not add any TV series yet!" << endl;
+    }
+    else
+    {
+        cout << favorites.getFullName() << " here is your favorite actors:" << endl;
+        favorites.displayFavoriteActors();
+    }
 }
 
 void mergeEpisodesAndSeries(const vector<episode> &episodesList, vector<series> &seriesList)
